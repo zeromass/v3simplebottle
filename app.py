d8179c6470ec6fa994a9b61572de4be7b28e7055
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-06-17 20:01:34 +0200

    Run server when file is executed as a script

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 __author__ = 'spousty'
 
 
-from bottle import route
+from bottle import route, run
 
 @route('/')
 def index(name):
     return "hello OpenShift Guru"
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080)
