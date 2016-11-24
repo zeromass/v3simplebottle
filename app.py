d6201daf35d41c553e90773e11729634e6272ecc
zeromass <zengenesis@gmail.com>
2016-11-24 13:10:45 +0200

    Changed message as per the openshift-all-in-one doc

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja</h1>"
+    return "<h1> hello OpenShift Ninja - you are doing well.</h1>"
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
